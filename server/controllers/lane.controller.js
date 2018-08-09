3f3b7c36880e2224a8802d5a767c410bb2c31364
Katarzyna Nieszporek <katarzyna.n.projekty@gmail.com>
2018-08-09 22:37:15 +0200

    Change lane.controller.js - get lane and delete lane

--- server/controllers/lane.controller.js
+++ server/controllers/lane.controller.js
@@ -4,7 +4,7 @@ import uuid from 'uuid';
 export function getSomething(req, res) {
   return res.status(200).end();
 }
-//Add Line
+//Add Lane
 export function addLane(req, res) {
   if (!req.body.name) {
     res.status(403).end();
@@ -20,5 +20,26 @@ export function addLane(req, res) {
       res.status(500).send(err);
     }
     res.json(saved);
+  });
+}
+//get lane
+export function getLanes(req, res) {
+  Lane.find().exec((err, lanes) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.json({ lanes });
+  });
+}
+//delete lane
+export function deleteLane(req, res) {
+  Lane.findOne({ id: req.params.laneId }).exec((err, lane) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+
+    lane.remove(() => {
+      res.status(200).end();
+    });
   });
 }
