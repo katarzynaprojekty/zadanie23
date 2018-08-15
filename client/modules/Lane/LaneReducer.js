fc0da04006a44afc4eda61d4413a3507de146e59
Katarzyna Nieszporek <katarzyna.n.projekty@gmail.com>
2018-08-15 22:51:16 +0200

    Add createNote, deleteNote

--- client/modules/Lane/LaneReducer.js
+++ client/modules/Lane/LaneReducer.js
@@ -1,5 +1,6 @@
 // Import Actions
 import { CREATE_LANE, UPDATE_LANE, DELETE_LANE } from './LaneActions';
+import { CREATE_NOTE, DELETE_NOTE } from '../Note/NoteActions';
 
 // Initial State
 const initialState = {};
@@ -16,6 +17,20 @@ const LaneReducer = (state = initialState, action) => {
 
     case DELETE_LANE:
       return state.filter(lane => lane.id !== action.laneId);
+
+    case CREATE_NOTE:
+      return state.map(lane => {
+        if (lane.id === action.laneId) {
+          const notes = [...lane.notes, action.note.id];
+          return { ...lane, notes };
+        }
+        return lane;
+      });
+
+    case DELETE_NOTE:
+      const updatedLane = { ...state[action.laneId] };
+      updatedLane.notes = updatedLane.notes.filter(noteId => noteId !== action.noteId);
+      return { ...state, [action.laneId]: updatedLane };
 
     default:
       return state;
