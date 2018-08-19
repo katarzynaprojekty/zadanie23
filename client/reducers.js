65598e5668ae9c73178d33318eb8ceeea9adefec
Katarzyna Nieszporek <katarzyna.n.projekty@gmail.com>
2018-08-20 00:47:06 +0200

    Update reducers.js

--- client/reducers.js
+++ client/reducers.js
@@ -8,6 +8,7 @@ import app from './modules/App/AppReducer';
 import lanes from './modules/Lane/LaneReducer';
 import notes from './modules/Note/NoteReducer';
 import intl from './modules/Intl/IntlReducer';
+import kanban from './modules/Kanban/KanbanReducer';
 
 // Combine all reducers into one root reducer
 export default combineReducers({
@@ -15,4 +16,5 @@ export default combineReducers({
   lanes,
   notes,
   intl,
+  kanban,
 });
