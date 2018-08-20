3640dca002ee554a4c69279001c520a8b7f7b025
Katarzyna Nieszporek <katarzyna.n.projekty@gmail.com>
2018-08-20 11:45:53 +0200

    Delete id in LaneActions.js

--- client/modules/Lane/LaneActions.js
+++ client/modules/Lane/LaneActions.js
@@ -1,5 +1,3 @@
-import uuid from 'uuid';
-
 import callApi from '../../util/apiCaller';
 import { lanes } from '../../util/schema';
 import { normalize } from 'normalizr';
@@ -17,7 +15,6 @@ export function createLane(lane) {
   return {
     type: CREATE_LANE,
     lane: {
-      id: uuid(),
       notes: [],
       ...lane,
     },
