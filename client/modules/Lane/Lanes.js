0eab42e890b87058d81abf0aa19f5d19923a038d
Katarzyna Nieszporek <katarzyna.n.projekty@gmail.com>
2018-08-20 13:49:13 +0200

    Change Lanes.js - add DnD

--- client/modules/Lane/Lanes.js
+++ client/modules/Lane/Lanes.js
@@ -2,16 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Lane from './LaneContainer.js';
 
-const Lanes = ({ lanes }) => {
+const Lanes = ({ lanes, moveNotes }) => {
   return (
     <div className="lanes">{lanes.map(lane =>
-      <Lane className="lane" key={lane.id} lane={lane} />
+      <Lane className="lane" key={lane.id} lane={lane} moveNotes={moveNotes}/>
     )}</div>
   );
 };
 
 Lanes.propTypes = {
   lanes: PropTypes.array,
+  moveNotes: PropTypes.func,
 };
 
 export default Lanes;
