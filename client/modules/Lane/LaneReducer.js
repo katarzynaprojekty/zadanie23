d4a55b6e308fd001c8a6f776414fcdaa9adb3fc1
Katarzyna Nieszporek <katarzyna.n.projekty@gmail.com>
2018-08-20 13:47:19 +0200

    Change LaneReducer.js - add DnD

--- client/modules/Lane/LaneReducer.js
+++ client/modules/Lane/LaneReducer.js
@@ -1,8 +1,17 @@
 // Import Actions
 import { CREATE_LANE, CREATE_LANES, UPDATE_LANE, DELETE_LANE, EDIT_LANE } from './LaneActions';
-import { DELETE_NOTE, CREATE_NOTE } from '../Note/NoteActions';
+import { DELETE_NOTE, CREATE_NOTE, MOVE_WITHIN_LANE } from '../Note/NoteActions';
 
 import omit from 'lodash/omit';
+
+function moveNotes(array, sourceNoteId, targetNoteId) {
+  const sourceIndex = array.indexOf(sourceNoteId);
+  const targetIndex = array.indexOf(targetNoteId);
+  const arrayCopy = [...array];
+
+  arrayCopy.splice(targetIndex, 0, arrayCopy.splice(sourceIndex, 1)[0]);
+  return arrayCopy;
+}
 
 // Initial State
 const initialState = {};
@@ -38,6 +47,13 @@ export default function lanes(state = initialState, action) {
 
     case DELETE_LANE: {
       return omit(state, action.laneId);
+    }
+
+    case MOVE_WITHIN_LANE: {
+      const newLane = { ...state[action.laneId] };
+      newLane.notes = moveNotes(newLane.notes, action.sourceId, action.targetId);
+    
+      return { ...state, [action.laneId]: newLane };
     }
 
     default:
